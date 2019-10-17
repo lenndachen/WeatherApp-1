ddf52154df3b76a01776af9ce82e9e98a657f78f
mamooredesigns <mamooredesigns@gmail.com>
2019-10-17 17:19:58 -0400

    added forecast info

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -9,11 +9,23 @@ class Home extends React.Component {
             temp: 0,
             condition: "",
             userEnteredZip: "",
+            date1: "",
+            lowTemp1: 0,
+            highTemp1: 0,
+            condition1: "",
+            date2: "",
+            lowTemp2: 0,
+            highTemp2: 0,
+            condition2: "",
+            date3: "",
+            lowTemp3: 0,
+            highTemp3: 0,
+            condition3: "",
         };
     }
 
     componentDidMount() {
-        // this.getWeather();
+        
     }
 
     getWeather() {
@@ -24,9 +36,8 @@ class Home extends React.Component {
         .then(response => response.json())
         .then(responseData => {
             console.log('My Weather Data', responseData);
-            const location = responseData.name;
             this.setState({ 
-                location: location,
+                location: responseData.name,
                 temp: responseData.main.temp,
                 condition: responseData.weather[0].description,
             });
@@ -48,34 +59,53 @@ class Home extends React.Component {
         })
     }
 
-    test = () => {
+    submit = () => {
         console.log("you clicked submit");
         this.getWeather();
+        this.getForecast();
     }
 
-    // getForecast() {
-    //     const apikey = process.env.REACT_APP_API_WEATHER_KEY;
-    //     const apiUrl = "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=" + apikey;
-    //     fetch( apiUrl )
-    //     .then(response => response.json())
-    //     .then(responseData => {
-    //         console.log('My Forecast Data', responseData);
-    //         // const forecastData = responseData.;
-    //         // this.setState({ forecastData }), () => {
-    //         // }
-    //     });
-    // }
+    getForecast() {
+        const apikey = process.env.REACT_APP_API_WEATHER_KEY;
+        let zipCode = this.state.userEnteredZip;
+        const apiUrl = "http://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us" + "&APPID=" + apikey;
+        fetch( apiUrl )
+        .then(response => response.json())
+        .then(responseData => {
+            console.log('My Forecast Data', responseData);
+            this.setState({ 
+                 date1: responseData.list[0].dt_txt,
+                 lowTemp1: responseData.list[0].main.temp_min,
+                 highTemp1: responseData.list[0].main.temp_max,
+                 condition1: responseData.list[0].weather[0].description,
+                 date2: responseData.list[8].dt_txt,
+                 lowTemp2: responseData.list[8].main.temp_min,
+                 highTemp2: responseData.list[8].main.temp_max,
+                 condition2: responseData.list[8].weather[0].description,
+                 date3: responseData.list[16].dt_txt,
+                 lowTemp3: responseData.list[16].main.temp_min,
+                 highTemp3: responseData.list[16].main.temp_max,
+                 condition3: responseData.list[16].weather[0].description,
+            });
+        });
+    }
 
     render() {
         return (
             <div className="homepage">
                 <h1>Weather App</h1>
-                <input onChange={this.handleUserEnteredZip} value={this.state.userEnteredZip} placeholder="Enter a zip code"></input> <button type="submit" onClick={() => this.test()}>Submit</button>
+                <input onChange={this.handleUserEnteredZip} value={this.state.userEnteredZip} placeholder="Enter a zip code"></input> <button type="submit" onClick={() => this.submit()}>Submit</button>
                 <br/>
                 <br/>
                 <div>Today's weather for {this.state.location}: </div>
                 <div> It is currently {this.convertToFarenheit(this.state.temp)} degrees Farenheit.</div>
                 <div> The conditions are: {this.state.condition}.</div>
+                <br/>
+                <br/>
+                <div>The forecast is: </div>
+                <div>For {this.state.date1}: a low temperature of {this.convertToFarenheit(this.state.lowTemp1)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp1)} with conditions of: {this.state.condition1}</div>
+                <div>For {this.state.date2}: a low temperature of {this.convertToFarenheit(this.state.lowTemp2)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp2)} with conditions of: {this.state.condition2}</div>
+                <div>For {this.state.date3}: a low temperature of {this.convertToFarenheit(this.state.lowTemp3)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp3)} with conditions of: {this.state.condition3}</div>
             </div>
         )
     }
