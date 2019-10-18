9d9a81e87d4cfff0accaeb0c26546b655fbd479f
mamooredesigns <mamooredesigns@gmail.com>
2019-10-18 12:36:00 -0400

    forecast updated to 5 days

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -22,6 +22,14 @@ class Home extends React.Component {
             lowTemp3: 0,
             highTemp3: 0,
             condition3: "",
+            date4: "",
+            lowTemp4: 0,
+            highTemp4: 0,
+            condition4: "",
+            date5: "",
+            lowTemp5: 0,
+            highTemp5: 0,
+            condition5: "",
         };
     }
 
@@ -69,6 +77,7 @@ class Home extends React.Component {
         const apikey = process.env.REACT_APP_API_WEATHER_KEY;
         let zipCode = this.state.userEnteredZip;
         const apiUrl = "http://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&APPID=" + apikey;
+        // const apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipCode + ",us&APPID=" + apikey;
         fetch( apiUrl )
         .then(response => response.json())
         .then(responseData => {
@@ -86,6 +95,14 @@ class Home extends React.Component {
                  lowTemp3: responseData.list[16].main.temp_min,
                  highTemp3: responseData.list[16].main.temp_max,
                  condition3: responseData.list[16].weather[0].description,
+                 date4: responseData.list[24].dt_txt,
+                 lowTemp4: responseData.list[24].main.temp_min,
+                 highTemp4: responseData.list[24].main.temp_max,
+                 condition4: responseData.list[24].weather[0].description,
+                 date5: responseData.list[32].dt_txt,
+                 lowTemp5: responseData.list[32].main.temp_min,
+                 highTemp5: responseData.list[32].main.temp_max,
+                 condition5: responseData.list[32].weather[0].description,
             });
         });
     }
@@ -135,10 +152,12 @@ class Home extends React.Component {
                 </div>
                 
                 <div className="forecast">
-                    <h2 className="foreHeader">The weekly forecast is: </h2>
+                    <h2 className="foreHeader">The 5-day forecast is: </h2>
                     <div className="foreDay1">For {this.state.date1}: a low temperature of {this.convertToFarenheit(this.state.lowTemp1)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp1)} with conditions of: {this.state.condition1}</div>
                     <div className="foreDay2">For {this.state.date2}: a low temperature of {this.convertToFarenheit(this.state.lowTemp2)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp2)} with conditions of: {this.state.condition2}</div>
                     <div className="foreDay3">For {this.state.date3}: a low temperature of {this.convertToFarenheit(this.state.lowTemp3)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp3)} with conditions of: {this.state.condition3}</div>
+                    <div className="foreDay4">For {this.state.date4}: a low temperature of {this.convertToFarenheit(this.state.lowTemp4)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp4)} with conditions of: {this.state.condition4}</div>
+                    <div className="foreDay5">For {this.state.date5}: a low temperature of {this.convertToFarenheit(this.state.lowTemp5)} degrees Farenheit and a high temperature of {this.convertToFarenheit(this.state.highTemp5)} with conditions of: {this.state.condition5}</div>
                 </div>
             </div>
         )
